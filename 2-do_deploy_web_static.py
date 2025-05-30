1c43d129850caa12cd57b1131332f91934b26e7b
isa-julie <j.isaro@alustudent.com>
2025-05-31 00:52:45 +0300

    new

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -1,33 +1,58 @@
 #!/usr/bin/python3
-"""Fabric script that distributes an archive to your web servers"""
-from fabric.api import env, put, run
+"""
+Fabric script that distributes an archive to web servers
+"""
+
+from fabric.api import env, put, run, sudo
 from os.path import exists
 
-env.hosts = ["52.91.2.150", "34.229.144.175"]
-env.user = "ubuntu"
-env.key = "~/.ssh/id_rsa"
+env.hosts = ['3.92.48.63', '44.202.237.194']  # Your actual server IPs
+env.user = 'ubuntu'
+# env.key_filename = '~/.ssh/id_rsa'  # Uncomment if using SSH key
 
 
 def do_deploy(archive_path):
-    """Function to distribute an archive to your web servers"""
+    """
+    Distributes an archive to web servers
+    
+    Args:
+        archive_path: Path to the archive to deploy
+        
+    Returns:
+        True if all operations succeeded, False otherwise
+    """
     if not exists(archive_path):
         return False
-    try:
-        file_name = archive_path.split("/")[-1]
-        name = file_name.split(".")[0]
-        path_name = "/data/web_static/releases/" + name
-        put(archive_path, "/tmp/")
-        run("mkdir -p {}/".format(path_name))
-        run('tar -xzf /tmp/{} -C {}/'.format(file_name, path_name))
-        run("rm /tmp/{}".format(file_name))
-        run("mv {}/web_static/* {}".format(path_name, path_name))
-        run("rm -rf {}/web_static".format(path_name))
-        run('rm -rf /data/web_static/current')
-        run('ln -s {}/ /data/web_static/current'.format(path_name))
-        return True
-    except Exception:
-        return False
 
-# Run the script like this:
-# $ fab -f 2-do_deploy_web_static.py
-# do_deploy:archive_path=versions/file_name.tgz
+    try:
+        # Upload archive
+        put(archive_path, "/tmp/")
+
+        # Extract archive filename and folder name
+        file_name = archive_path.split("/")[-1]
+        folder_name = file_name.split(".")[0]
+        release_path = "/data/web_static/releases/{}".format(folder_name)
+
+        # Create target directory
+        run("mkdir -p {}".format(release_path))
+
+        # Uncompress archive
+        run("tar -xzf /tmp/{} -C {}".format(file_name, release_path))
+
+        # Remove archive
+        run("rm /tmp/{}".format(file_name))
+
+        # Move contents and remove web_static folder
+        run("mv {}/web_static/* {}".format(release_path, release_path))
+        run("rm -rf {}/web_static".format(release_path))
+
+        # Update symbolic link
+        run("rm -rf /data/web_static/current")
+        run("ln -s {} /data/web_static/current".format(release_path))
+
+        print("New version deployed!")
+        return True
+
+    except Exception as e:
+        print("Deployment failed:", str(e))
+        return False
